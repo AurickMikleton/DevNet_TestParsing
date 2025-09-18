9c31f3f74f4e936c05d81a82eff48b1b8ebdef10
Ruhan Madhu <Ruhan-Madhu@outlook.com>
2025-09-18 13:54:43 -0600

    made code adhere to good practices

--- src/main.py
+++ src/main.py
@@ -2,27 +2,32 @@ import sys
 import os
 import text_parsing
 
-#book_file = open("../data/book.txt", 'r', encoding='utf-8')
-#print(text_parsing.parse(book_file.read()))
-#book_file.close()
+# book_file = open("../data/book.txt", 'r', encoding='utf-8')
+# print(text_parsing.parse(book_file.read()))
+# book_file.close()
+
 
 def main():
     print(len(sys.argv))
-    if len(sys.argv) != 3: show_docs()
+    if len(sys.argv) != 3:
+        show_docs()
     if sys.argv[1] != "parse":
         print(f"Invalid command: {sys.argv[1]}")
         show_docs()
-    if os.path.isfile(sys.argv[2]) != True:
+    if os.path.isfile(sys.argv[2]) is False:
         print("Invalid file path")
         quit()
     book_file = open(sys.argv[2], 'r', encoding='utf-8')
     print(text_parsing.parse(book_file.read()))
     book_file.close()
 
+
 def show_docs():
-    print("-------- Valid Commands --------\n"\
+    print("-------- Valid Commands --------\n"
           " parse <path>  Parse the text in a txt file"
-         )
+          )
     quit()
-     
-main()
+
+
+if __name__ == "__main__":
+    main()
