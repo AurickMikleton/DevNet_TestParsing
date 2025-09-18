6891c0edc2401927b1f18392952962effb49b8e7
AurickMikleton <228803305+AurickMikleton@users.noreply.github.com>
2025-09-17 20:46:31 -0600

    added command line arguments to select the parsed text file

--- src/main.py
+++ src/main.py
@@ -1,7 +1,28 @@
+import sys
+import os
 import text_parsing
 
-book_file = open("../data/book.txt", 'r', encoding='utf-8')
+#book_file = open("../data/book.txt", 'r', encoding='utf-8')
+#print(text_parsing.parse(book_file.read()))
+#book_file.close()
 
-print(text_parsing.parse(book_file.read()))
+def main():
+    print(len(sys.argv))
+    if len(sys.argv) != 3: show_docs()
+    if sys.argv[1] != "parse":
+        print(f"Invalid command: {sys.argv[1]}")
+        show_docs()
+    if os.path.isfile(sys.argv[2]) != True:
+        print("Invalid file path")
+        quit()
+    book_file = open(sys.argv[2], 'r', encoding='utf-8')
+    print(text_parsing.parse(book_file.read()))
+    book_file.close()
 
-book_file.close()
+def show_docs():
+    print("-------- Valid Commands --------\n"\
+          " parse <path>  Parse the text in a txt file"
+         )
+    quit()
+     
+main()
--- src/text_parsing.py
+++ src/text_parsing.py
@@ -1,11 +1,6 @@
 def parse(text):
     initial_asterisk = text.find("***")
     header_cutoff = text.find("\n", initial_asterisk)
-    print(header_cutoff)
-    #footer_asterisk = text.find("***", header_cutoff)
     footer_cutoff = text.find("***", header_cutoff + 1)
-    print(footer_cutoff)
-
     paragraphs = text[header_cutoff:footer_cutoff].split('\n\n')
     return [i for i in paragraphs if i.strip()]
-    #return [i.strip() != "" for i in paragraphs]
