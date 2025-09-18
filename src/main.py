6f211095990f8f8c570cff4a370caf528bca9f4c
Ruhan Madhu <Ruhan-Madhu@outlook.com>
2025-09-17 20:12:17 -0600

    parser filters out empty strings

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 import text_parsing
+import sys
 
 book_file = open("../data/book.txt", 'r', encoding='utf-8')
 
 print(text_parsing.parse(book_file.read()))
+print(sys.argv[1:])
 
 book_file.close()
--- src/text_parsing.py
+++ src/text_parsing.py
@@ -2,8 +2,8 @@ def parse(text):
     initial_asterisk = text.find("***")
     header_cutoff = text.find("\n", initial_asterisk)
     print(header_cutoff)
-    #footer_asterisk = text.find("***", header_cutoff)
+#   footer_asterisk = text.find("***", header_cutoff)
     footer_cutoff = text.find("***", header_cutoff + 1)
     print(footer_cutoff)
 
-    return text[header_cutoff:footer_cutoff].split('\n\n')
+    return [char for char in (text[header_cutoff:footer_cutoff].split('\n\n')) if char != '']
