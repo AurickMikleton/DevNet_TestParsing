33dfa1f8913125000e589b94a07b083d492b2f3b
AurickMikleton <228803305+AurickMikleton@users.noreply.github.com>
2025-09-17 19:36:35 -0600

    fixed parsing to removed header and footer

--- src/text_parsing.py
+++ src/text_parsing.py
@@ -1,2 +1,9 @@
 def parse(text):
-    return text.split('\n\n')
+    initial_asterisk = text.find("***")
+    header_cutoff = text.find("\n", initial_asterisk)
+    print(header_cutoff)
+    #footer_asterisk = text.find("***", header_cutoff)
+    footer_cutoff = text.find("***", header_cutoff + 1)
+    print(footer_cutoff)
+
+    return text[header_cutoff:footer_cutoff].split('\n\n')
