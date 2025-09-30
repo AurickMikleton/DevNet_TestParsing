9a5e59139300628b3fbaae618f705e635784e3f1
AurickMikleton <228803305+AurickMikleton@users.noreply.github.com>
2025-09-30 17:40:48 -0600

    initial version of parse executable

---
+++ csrc/parse.c
@@ -0,0 +1,65 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+
+#define BUFFER_SIZE 64
+
+bool endOfWord(char c) {
+    if (c == ' ' || c == ',' || c == '\n') return true;
+    if (c == '.' || c == '!' || c == '?') return true;
+    return false;
+}
+
+void parse(FILE *file, char* name) { //count one "***" as a word
+    FILE *newFile;
+    char newFileName[256] = "parsed/";
+    strcat(newFileName, name);
+    newFile = fopen("burito.txt", "w");
+    if (newFile == NULL) {
+        perror("Error parsing file");
+        return;
+    }
+
+    char currentWord[BUFFER_SIZE];
+    char c;
+    int i = 0;
+    bool textInBook = false;
+    int astriskTagCount = 0;
+
+    while ((c = fgetc(file)) != EOF) {
+        c = tolower(c);
+        currentWord[i] = '\0';
+        if (textInBook) fputc(c, newFile);
+        if (strcmp(currentWord, "***") == 0) {
+            astriskTagCount += 1;
+            if (textInBook) {
+                break;
+            }
+            else if (astriskTagCount > 1){
+                textInBook = true;
+            }
+        }
+        if (endOfWord(c)) {
+            if (i == 0) continue;
+            i = 0;
+            continue;
+        }
+        currentWord[i] = c;
+        i++;
+    }
+    fclose(newFile);
+}
+
+int main(int argc, char** argv) {
+    FILE *file;
+    file = fopen(argv[1], "r");
+    if (file == NULL) {
+        perror("Error parsing file");
+        return EXIT_FAILURE;
+    }
+    parse(file, argv[1]);
+    fclose(file);
+    return EXIT_SUCCESS;
+}
