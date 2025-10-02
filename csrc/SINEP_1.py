c28abdfd1367fdcf6f6fad0946a37f3f04b8cd67
Ruhan Madhu <Ruhan-Madhu@outlook.com>
2025-10-01 18:18:49 -0600

    everything works now yayyyy

--- csrc/SINEP_1.py
+++ csrc/SINEP_1.py
@@ -4,18 +4,21 @@ import subprocess
 
 os.makedirs("parsed", exist_ok=True)
 
+
 def parse():
     files = os.listdir(sys.argv[2])
     for i in files:
         path = sys.argv[2] + '/' + i
-        result = subprocess.run(["parse.exe", path, i])
+        result = subprocess.run(["./parse.exe", path, i])
+
 
 def stat():
     banlist = sys.argv[2]
     files = os.listdir("parsed")
     for i in files:
         path = "parsed/" + i
-        result = subprocess.run(["stat.exe", path, banlist, i])
+        result = subprocess.run(["./stat.exe", path, banlist, i])
+
 
 def main():
     if len(sys.argv) != 3:
@@ -27,11 +30,13 @@ def main():
             print(f"Invalid command: {sys.argv[1]}")
             show_docs()
 
+
 def show_docs():
     print("-------- Valid Commands --------\n"
           " parse <dir path>  Parse the text in the files within the provided directory"
           " stat <file path>  File to a csv 'banlist' print stats of parsed files"
           )
     quit()
+
 
 main()
